4505141f8b2255398630339c3d950fd46f96f1fd
Prashant <praytoprashant@gmail.com>
2024-01-02 00:11:38 +0530

    3rd commit with trending,switchingtabs,carousel etc...

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import  { useState,useEffect } from 'react';
 import {BrowserRouter,Route,Routes} from "react-router-dom"
 import { fetchDataFromApi } from './utils/api';
 import { useSelector, useDispatch } from 'react-redux'
-import { getApiConfiguration } from './store/homeSlice';
+import { getApiConfiguration,getGenres } from './store/homeSlice';
 
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
@@ -20,31 +20,46 @@ function App() {
 
   useEffect (() =>{
     fetchApiConfig();
-  })
+    genresCall();
+  },[])
 
   const fetchApiConfig= () => {
-    fetchDataFromApi('/configuration')
-      .then((res) => {
+    fetchDataFromApi('/configuration').then((res) => {
         console.log(res);
 
         const url ={
           backdrop: res.images.secure_base_url + "original",
-          poster : res.images.secure_base_url + "original",
-          profile : res.images.secure_base_url + "original"
+          poster: res.images.secure_base_url + "original",
+          profile: res.images.secure_base_url + "original"
 
 
         }
-
-
-
         dispatch(getApiConfiguration(url))
       });
   };
 
+  const genresCall = async ()=>{
+    let promises = []
+    let endPoints = [ "tv", "movie"]
+    let allGenres ={}
+
+    endPoints.forEach((url) =>{
+      promises.push(fetchDataFromApi(`/genre/${url}/list`))
+    })
+
+    const data = await Promise.all (promises);
+    data.map (({genres}) =>{
+      return genres.map((item) => {allGenres[item.id] = item})
+    });
+
+    dispatch (getGenres(allGenres));
+    
+  }
+
 
 
   return (<BrowserRouter>
-  {/* <Header/> */}
+  <Header/>
   <Routes>
     <Route path="/" element={<Home />}/>
     <Route path="/:mediaType/:id" element={<Details />} />
@@ -52,7 +67,7 @@ function App() {
     <Route path="/explore/:mediaType" element={<Explore />} />
     <Route path="*" element={<PageNotFound />} />
   </Routes>
-   {/* <Footer/> */}
+   <Footer/>
   </BrowserRouter>
 
   );
--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,50 +1,60 @@
-import React,{useState, useEffect} from 'react'
-import { useNavigate } from 'react-router-dom';
-import useFetch from '../../../hooks/useFetch';
-import { UseSelector, useSelector } from 'react-redux/es/hooks/useSelector';
-import "./style.scss"
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import useFetch from "../../../hooks/useFetch";
+import { useSelector } from "react-redux";
+import "./style.scss";
+import Img from "../../../components/lazyLoadImage/Img";
+import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 
 function HeroBanner() {
-  const [background,setBackground]=useState ("");
-  const [query,setQuery]=useState("");
+  const [background, setBackground] = useState("");
+  const [query, setQuery] = useState("");
   const navigate = useNavigate();
-  const {url}= useSelector ((state) => state.home);
-  const {data,loading} = useFetch("/movie/upcoming");
-  useEffect(() =>{
-        const bg = url.backdrop +
-        data?.results?.[Math.floor(Math.random()*20)]?.backdrop_path;
-        setBackground(bg);
+  const { url } = useSelector((state) => state.home);
+  const { data, loading } = useFetch("/movie/upcoming");
+  useEffect(() => {
+    const bg =
+      url.backdrop +
+      data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path;
+    setBackground(bg);
   }, [data]);
 
-  const searchQueryHandler =(event) =>{
-    if(event.key==="Enter" && query.length>0 ){
-      navigate(`/search/${query}`)
-
-    };
-  }
+  const searchQueryHandler = (event) => {
+    if (event.key === "Enter" && query.length > 0) {
+      navigate(`/search/${query}`);
+    }
+  };
 
   return (
-    <div className='heroBanner'>
-      <div className="wrapper">
-        <div className="heroBannerContent">
-          <span className="title">WELCOME.</span>
-          <span className="subTitle">Millions of movies,TV shpws and people to discover. 
-          Explore now.
-           </span>
-           <div className="searchInput">
-               <input
-                type="text"
-               placeholder='search for a movie or tv show...' 
-               onChange={(e)=>setQuery(e.target.value)}
-               onKeyUp={(searchQueryHandler)}
-               
-               />  
-               <button>Search</button>
-           </div>
+    <div className="heroBanner">
+      {!loading && (
+        <div className="backdrop-img">
+          <Img src={background} />
         </div>
-      </div>
+      )}
+          <div className="opacity-layer"></div>
+
+      <ContentWrapper>
+        <div className="wrapper">
+          <div className="heroBannerContent">
+            <span className="title">WELCOME.</span>
+            <span className="subTitle">
+              Millions of movies,TV shows and people to discover. Explore now.
+            </span>
+            <div className="searchInput">
+              <input
+                type="text"
+                placeholder="search for a movie or tv show..."
+                onChange={(e) => setQuery(e.target.value)}
+                onKeyUp={searchQueryHandler}
+              />
+              <button>Search</button>
+            </div>
+          </div>
+        </div>
+      </ContentWrapper>
     </div>
-  )
+  );
 }
 
-export default HeroBanner
+export default HeroBanner;
