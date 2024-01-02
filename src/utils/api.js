050e9ba8533e440ede1173eea56df7756d1862e3
Prashant <praytoprashant@gmail.com>
2024-01-02 11:59:21 +0530

    proper commets are add to src/utils/api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,20 +1,35 @@
+// Importing Axios library for making HTTP requests
 import axios from "axios";
 
+// Base URL for The Movie Database API
 const BASE_URL = "https://api.themoviedb.org/3";
+
+// Token for authenticating requests to The Movie Database API
 const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
 
+// Headers containing the authorization token
 const headers = {
   Authorization: "bearer " + TMDB_TOKEN,
 };
 
+/**
+ * Function to fetch data from The Movie Database API
+ * @param {string} url - The endpoint URL for the API request
+ * @param {Object} params - Optional parameters for the API request
+ * @returns {Promise<Object>} - A promise resolving to the fetched data or an error object
+ */
 export const fetchDataFromApi = async (url, params) => {
   try {
+    // Making a GET request using Axios with the provided URL, headers, and parameters
     const { data } = await axios.get(BASE_URL + url, {
       headers,
       params
     });
+
+    // Returning the fetched data
     return data;
   } catch (err) {
+    // Logging and returning any error that occurs during the API request
     console.error(err);
     return err;
   }
